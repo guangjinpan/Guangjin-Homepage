e7dbe970c9a52c48708627ac2201c27a1cf5cdf3
guangjinpan <35365588+guangjinpan@users.noreply.github.com>
2025-05-28 20:32:42 +0000

    Deploying to gh-pages from @ guangjinpan/Guangjin-Homepage@9a8bfb423599378dd8b5310ca7a9b0d59205c4c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -394,15 +394,15 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-2024-news",
+          title: '2024 News',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/Guangjin-Homepage/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
