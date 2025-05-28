591f0a52663032137eacbf949ffe056284296d6b
guangjinpan <35365588+guangjinpan@users.noreply.github.com>
2025-05-28 19:54:58 +0000

    Deploying to gh-pages from @ guangjinpan/Guangjin-Homepage@405e50fa667bf358ec05c1e82a4a4ddba11b91b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Guangjin-Homepage/repositories/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/Guangjin-Homepage/people/";
+            window.location.href = "/Guangjin-Homepage/cv/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
@@ -44,14 +44,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/Guangjin-Homepage/blog/";
               },
-            },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/Guangjin-Homepage/cv/";
-          },
-        },{id: "nav-projects",
+            },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
